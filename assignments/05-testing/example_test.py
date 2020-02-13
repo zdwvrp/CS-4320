96ee6cbb0c0f84e16de26f408351487ede19a65e
Caleb Heinzman <caleb.heinzman@gmail.com>
2020-02-13 15:05:13 -0600

    Add files via upload

---
+++ assignments/05-testing/example_test.py
@@ -0,0 +1,15 @@
+import pytest
+import System
+
+#Tests if the program can handle a wrong username
+def test_login(grading_system):
+    username = 'thtrhg'
+    password =  'fhjhjdhjdfh'
+    grading_system.login(username,password)
+
+
+@pytest.fixture
+def grading_system():
+    gradingSystem = System.System()
+    gradingSystem.load_data()
+    return gradingSystem
